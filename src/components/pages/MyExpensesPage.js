04c7aabb51003c568684e63ee77ebb1e531eb31a
daniel-saghy <daniel.saghy@sogeti.com>
2023-10-12 17:24:51 +0300

    formatting styled elements

--- src/components/pages/MyExpensesPage.js
+++ src/components/pages/MyExpensesPage.js
@@ -7,16 +7,26 @@ import {
     Container,
     Button,
     ListItemButton,
-    ListItemText 
+    ListItemText, 
+    Grid,
+    Box
 } from '@mui/material';
 
 
 const Wrapper = styled(Container)(() => ({
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
-    marginTop: '50px'
+    alignItems: 'center',    
+    justifyContent: 'center',
+    marginTop: '50px',    
+    minHeight: '50vh',
   }));
+
+const ContentContainer = styled(Box)(() => ({
+    marginTop: '30px',
+    maxWidth: '800px',
+    width: '100%',
+}));
 
 const StyledCard = styled(Card)(({ theme }) => ({
     background: theme.palette.secondary.main,
@@ -24,26 +34,41 @@ const StyledCard = styled(Card)(({ theme }) => ({
     marginBottom: '20px',
   }));
 
+const StyledButton = styled(Button)(( ) => ({
+    height: '100px',
+    width: '100%',
+}));
+
 export const MyExpensesPage = () => {
     const { t } = useTranslation();
 
     return (
         <Wrapper>
-            <Typography variant="h1">{t('myExpenses.title')}</Typography>
-
-            <StyledCard>
-                <Button variant="text">{t('myExpenses.addNew')}</Button>
-            </StyledCard>
-    
-            <StyledCard>
-                <Button variant="text">{t('myExpenses.totalBalance')}</Button>
-            </StyledCard>
-    
-            <StyledCard>
-                <ListItemButton component="a" href="#simple-list">
-                    <ListItemText primary="Spotify - 400€" />
-                </ListItemButton>
-            </StyledCard>
+            <Typography variant="h1">{t('myExpenses.title')}</Typography>            
+            <ContentContainer>                
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <StyledCard>
+                            <StyledButton variant="text">{t('myExpenses.addNew')}</StyledButton>
+                        </StyledCard>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <StyledCard>
+                            <StyledButton variant="text">{t('myExpenses.totalBalance')}</StyledButton>
+                        </StyledCard>
+                    </Grid>
+                </Grid>
+        
+                <Grid container spacing={1}>
+                    <Grid item xs={12}>
+                        <StyledCard>
+                            <ListItemButton component="a" href="#simple-list">
+                                <ListItemText primary="Spotify - 400€" />
+                            </ListItemButton>
+                        </StyledCard>
+                    </Grid>
+                </Grid>
+            </ContentContainer>
       </Wrapper>
     );
   };
