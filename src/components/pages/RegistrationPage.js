06a1212a6c380c9491b14a523fb3c5062779ee80
Hilda Palva <hilupalva@gmail.com>
2023-10-22 12:36:41 +0300

    add persons

--- src/components/pages/RegistrationPage.js
+++ src/components/pages/RegistrationPage.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useTranslation} from 'react-i18next'
 import {
     Button,
     Container, 
-    Grid, 
-    styled, 
+    Grid,  
     FormLabel, 
+    styled,
     Input, 
     FormGroup, 
     Typography
 } from '@mui/material';
-import paths from '../../utilities/pathnames'
+//import paths from '../../utilities/pathnames'
 
 // Styles
 const Wrapper = styled(Container)(() => ({
@@ -47,9 +47,42 @@ const Wrapper = styled(Container)(() => ({
       background: theme.palette.primary.blue
     }
   }));
+
+
   
   export const RegistrationPage = () => {
     const { t } = useTranslation();
+
+    const [people, setPeople] = useState([])
+    const [newPerson, setNewPerson] = useState({email: '', password:'', retypePassword:''})
+    const [passwordMismatch, setPasswordMismatch] = useState(false)
+    const [registrationDone, setRegistrationDone] = useState(false)
+
+    const handleEmailChange = (e) => {
+      setNewPerson({...newPerson, email: e.target.value})
+    }
+
+    const handlePasswordChange = (e) => {
+      setNewPerson({...newPerson, password: e.target.value})
+    }
+
+    const handleRetypePasswordChange = (e) => {
+      setNewPerson({...newPerson, retypePassword: e.target.value})
+    }
+
+    const handleSubmit = () => {
+      if (newPerson.password == newPerson.retypePassword) {
+        setPeople([...people, newPerson])
+        setNewPerson({email: '', password: '', retypePassword: ''})
+        setPasswordMismatch(false)
+        setRegistrationDone(true)
+        console.log('Submitting registration done')
+        localStorage.setItem('people', JSON.stringify(people))
+      } else {
+        setPasswordMismatch(true)
+        setRegistrationDone(false)
+      }
+    }
   
     return (
       <Wrapper>
@@ -59,18 +92,20 @@ const Wrapper = styled(Container)(() => ({
             <FormLabel>
               <Typography variant="h6">{t('registration.email')}</Typography>
             </FormLabel>
-            <InputLine id="settings_email" />
+            <InputLine id="settings_email" value={newPerson.email} onChange={handleEmailChange}/>
             <FormLabel>
               <Typography variant="h6">{t('registration.password')}</Typography>
             </FormLabel>
-            <InputLine id="settings_psw" />
+            <InputLine id="settings_psw" type="password" value={newPerson.password} onChange={handlePasswordChange}/>
             <FormLabel>
               <Typography variant="h6">{t('registration.retypePassword')}</Typography>
             </FormLabel>
-            <InputLine id="settings_retype_psw" />
-            <StyledButton href={paths.profile.path}>
+            <InputLine id="settings_retype_psw" type="password" value={newPerson.retypePassword} onChange={handleRetypePasswordChange}/>
+            {passwordMismatch && <p style={{ color: 'red' }}>Passwords do not match.</p>}
+            <StyledButton onClick={handleSubmit}>
               <Typography variant="h6">{t('registration.save')}</Typography>
             </StyledButton>
+            {registrationDone && <p style={{ color: 'green' }}>Registration succesfull.</p>}
           </FormGroup>
         </FormWrapper>
       </Wrapper>
