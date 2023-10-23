9dd735d5b9b15f8e1db93e717d004d3e461dde59
Julie Pražáková <xprazak6@mendelu.cz>
2023-10-23 10:22:32 +0300

    changed the background

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header/header';
-import { LayoutContainer } from './LayoutStyles';
+import { LayoutContainer, LowerLeftWave, UpperRightWave } from './LayoutStyles';
 
 export const Layout = () => {
   return (
     <>
       <LayoutContainer>
+        <LowerLeftWave size="200px" />
+        <UpperRightWave size="200px"/>
         <Header />
         <Outlet />
       </LayoutContainer>
