ec025055e535e88fc37abb78537045bbaa6ebbe1
daniel-saghy <daniel.saghy@sogeti.com>
2023-12-01 15:13:15 +0200

    add your forst transaction

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Grid, styled, Typography, Container } from '@mui/material';
+import { Grid, styled, Typography, Container, Button } from '@mui/material';
 import 'react-calendar/dist/Calendar.css';
 import LineChart from '../common/LineChart';
 import { MCalendar } from '../common/Calendar';
@@ -94,6 +94,19 @@ const MLink = styled(Link)(({ theme }) => ({
   }
 }));
 
+const MButton = styled(Button)(() => ({
+  height: '120px',
+  width: '100%',
+  justifyContent: 'center'
+}));
+
+const StyledDiv = styled('div')(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center'
+}));
+
 export const MainPage = () => {
   const { t } = useTranslation();
   const [greeting, setGreeting] = useState('');
@@ -145,31 +158,42 @@ export const MainPage = () => {
         <Greeting variant="h1">{greeting}</Greeting>
         <Welcome variant="h3">{t('specific.welcome')}</Welcome>
       </MTitle>
-      <div>
-        {expenses.length !== 0 || incomes.length !== 0 ? (
-          <LineChart expenses={expenses} incomes={incomes} />
-        ) : (
-          <div> No data to display </div>
-        )}
-        <Box>
-          <MCalendar />
+      {expenses.length === 0 && incomes.length === 0 ? (
+        <StyledDiv>
+          <h3> Please add your first transaction... </h3>
+          <Link to={paths.transactions.path}>
+            <MButton variant="text">
+              <Typography variant="h2">{t('transactions.addNew')}</Typography>
+            </MButton>
+          </Link>
+        </StyledDiv>
+      ) : (
+        <div>
           {expenses.length !== 0 || incomes.length !== 0 ? (
-            <DoughnutChart expenses={expenses} incomes={incomes} />
+            <LineChart expenses={expenses} incomes={incomes} />
           ) : (
             <div> No data to display </div>
           )}
-        </Box>
-        <MBox>
-          <ActionButtons />
-          <MLink to={paths.transactions.path}>
+          <Box>
+            <MCalendar />
             {expenses.length !== 0 || incomes.length !== 0 ? (
-              <Transactions expenses={expenses} incomes={incomes} />
+              <DoughnutChart expenses={expenses} incomes={incomes} />
             ) : (
               <div> No data to display </div>
             )}
-          </MLink>
-        </MBox>
-      </div>
+          </Box>
+          <MBox>
+            <ActionButtons />
+            <MLink to={paths.transactions.path}>
+              {expenses.length !== 0 || incomes.length !== 0 ? (
+                <Transactions expenses={expenses} incomes={incomes} />
+              ) : (
+                <div> No data to display </div>
+              )}
+            </MLink>
+          </MBox>
+        </div>
+      )}
     </Wrapper>
   );
 };
